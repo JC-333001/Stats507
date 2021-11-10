4860a6ec94a304b5f81f8f7d9cbef325045e6362
Jiaxi Chen <jxichen@umich.edu>
2021-11-10 14:01:30 -0500

    Update p2.py

--- ps2.py
+++ ps2.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from IPython.core.display import display, Markdown
-import time
+import math
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from pydataset import data
+from scipy.stats import ttest_ind_from_stats
+from IPython.core.display import display, HTML
+from scipy.stats import norm
 
-# ## Question 3
-
-# ### a.
-
-# +
 cohort_1 = pd.read_sas('https://wwwn.cdc.gov/Nchs/Nhanes/2011-2012/DEMO_G.XPT')
 cohort_1['cohort'] = '2011-2012'
 
@@ -19,17 +19,17 @@ cohort_3['cohort'] = '2015-2016'
 
 cohort_4 = pd.read_sas('https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DEMO_J.XPT')
 cohort_4['cohort'] = '2017-2018'
-
-df = pd.concat([cohort_1, cohort_2, cohort_3, cohort_4])
-df = df[['SEQN', 'RIDAGEYR', 'RIDRETH3', 'DMDEDUC2', 'DMDMARTL', 'RIDSTATR',
-                    'SDMVPSU', 'SDMVSTRA', 'WTMEC2YR', 'WTINT2YR', 'cohort']]
 # -
 
+df = pd.concat([cohort_1, cohort_2, cohort_3, cohort_4])
+df = df[['SEQN', 'RIDAGEYR', 'RIAGENDR', 'RIDRETH3', 'DMDEDUC2', 'DMDMARTL',
+         'RIDSTATR','SDMVPSU', 'SDMVSTRA', 'WTMEC2YR', 'WTINT2YR', 'cohort']]
 df1 = df.rename(columns={"SEQN": "id", "RIDAGEYR": "age",
+                         "RIAGENDR" : "gender",
                          "RIDRETH3": "race_and_ethnicity",
                          "DMDEDUC2": "education",
                          "DMDMARTL": "marital_status",
-                         "RIDSTATR": "interview_exam_status",
+                         "RIDSTATR": "exam_status",
                          "SDMVPSU": "masked_variance_PSU",
                          "SDMVSTRA": "masked_variance_stratum",
                          "WTMEC2YR": "interviewed_and_MEC_examined",
@@ -40,9 +40,10 @@ df1 = df1.fillna(-1)
 # and 'masked_variance_stratum' to integer
 df1[['id', 'age', 'education',
      'masked_variance_stratum',
-     'masked_variance_PSU']] = df1[["id", "age", 'education',
+     'masked_variance_PSU','exam_status']] = df1[["id", "age", 'education',
                                     'masked_variance_stratum',
-                                    'masked_variance_PSU']].astype(int)
+                                    'masked_variance_PSU',
+                                    'exam_status']].astype(int)
 df1 = df1.set_index('id')
 # Create categorical variable for race_and_ethnicity
 replace_map = {'race_and_ethnicity': {1: 'Mexican_American',
@@ -58,15 +59,16 @@ replace_map = {'race_and_ethnicity': {1: 'Mexican_American',
                                  -1: 'Missing'},
               'interview_exam_status': {1: 'Interviewed Only',
                                         2: 'Both interviewed and MEC examined',
-                                        -1: 'Missing'}}
+                                        -1: 'Missing'},
+              'gender': {1: 'male', 2: 'female'}}
 df1_replace = df1.copy()
 df1_replace.replace(replace_map, inplace=True)
 df1_replace.head(10)
 
+df1_replace.to_pickle("demographic.pkl")
+df1_read = pd.read_pickle('demographic.pkl')
 
-df1_replace.to_pickle("/Users/jiaxichen/Desktop/STATS507/HW/demographic.pkl")
-
-# ### b.
+# ### part b)
 
 # +
 cohort_health_1 = pd.read_sas('https://wwwn.cdc.gov/Nchs/Nhanes/2011-2012/'\
@@ -88,69 +90,77 @@ cohort_health_4['cohort'] = '2017-2018'
 # +
 df_health = pd.concat([cohort_health_1, cohort_health_2,
                        cohort_health_3, cohort_health_4])
-var_names = list(df_health)
-var_names = [var for var in var_names
-             if var[0:3] == 'OHX' and (var[-3:] == 'CTC'
-                                       or (var[-2:] == 'TC'
-                                           and var[-3] != 'R'))]
-var_names.extend(['SEQN', 'OHDDESTS', 'cohort'])
-
-df_health = df_health[var_names]
-
-# +
-df1_health = df_health.rename(columns={"SEQN": "id"})
-# Convert all missing values to be -1
+df1_health = df_health.rename(columns={"SEQN": "id",
+                                       "OHDDESTS": "ohx_status"})
 df1_health = df1_health.fillna(-1)
-#convert "id", "age", 'education', 'masked_variance_PSU', and
-# 'masked_variance_stratum' to integer
 df1_health[['id']] = df1_health[["id"]].astype(int)
 df1_health = df1_health.set_index('id')
-# Create categorical variable for race_and_ethnicity
-replace_map1 = {1: 'Primary Tooth', 2: 'Permanent tooth present',
-                3: 'Dental implant', 4: 'Tooth not present',
-                5: 'Permanent dental root fragment present',
-                9: 'Could not assess', -1: 'Missing'}
-replace_map2 = {1: 'Complete', 2:'Partial', 3:'Not Done', -1: 'Missing'}
-df1_health_replace = df1_health.copy()
-TC_replace = df1_health_replace[[var for var in var_names
-                                 if var[0:3] == 'OHX'
-                                 and (var[-3] != 'C'
-                                      and var[-2:] == 'TC')]].replace(
-    replace_map1, inplace=False)
-CTC_replace = df1_health_replace[[var for var in var_names
-                                  if var[0:3] == 'OHX'
-                                  and var[-3:] == 'CTC']]
-CTC_replace = CTC_replace.applymap(lambda x: x.decode("utf-8"))
-CTC_replace = CTC_replace.applymap(lambda x: 'Missing' if x == '' else x)
 
-TS_replace = df1_health_replace[['OHDDESTS', 'cohort']].replace(
-    replace_map2, inplace=False)
-df1_health_concat = pd.concat([TC_replace, CTC_replace, TS_replace], axis = 1)
+df1_left = df1_read[['gender', 'age', 'exam_status', 'education']]
+df1_left['under_20'] = np.where(df1_left['age'] < 20, True, False)
+df1_left['college'] = ["some college/college graduate"
+                       if (i == 4 or i == 5) and j == False
+                       else "No college/<20"
+                       for (i,j) in
+                       zip(df1_left['education'],df1_left['under_20'])]
+df1_right = df1_health['ohx_status']
+df1_join = df1_left.join(df1_right, how='left')
+df1_join = df1_join.fillna(-1)
+df1_join['ohx_status'] = df1_join['ohx_status'].astype(int)
 
-# +
-df1_health_rename = df1_health_concat.copy()
-for i in range(0,32):
-    df1_health_rename.columns.values[i] = 'tooth_count_{}'.format(i+1)
-
-for i in range(32,60):
-    df1_health_rename.columns.values[i] = 'coronal_caries_{}'.format(i-28)
-
-df1_health_rename.columns.values[-2] = 'dentition_status'
-
-df1_health_rename.head(10)
+df1_join['ohx'] = ['complete' if i == 2 and j == 1
+                   else 'missing'
+                   for (i,j) in
+                   zip(df1_join['exam_status'], df1_join['ohx_status'])]
+df1_final = df1_join[['gender', 'age', 'under_20',
+                      'college', 'exam_status',
+                      'ohx_status', 'ohx']]
+df1_final.to_pickle("dentition.pkl")
 # -
 
-df1_health_concat.to_pickle("/Users/jiaxichen/Desktop/STATS507/HW/"\
-                            "dentition.pkl")
+df1_final
 
-# ### c.
+# ### part c)
 
-print("shape of demographic data in part a: ", df1_replace.shape)
-print("shape of dentition data in part b: ", df1_health_concat.shape)
+df2 = pd.read_pickle('dentition.pkl')
 
-# - The demographic dataset in part(a) has 39156 cases
-# - The dentition dataset in part(b) has 35909 cases
+# Number of rows needed to be dropped
+dropped = df2.loc[df2['exam_status'] != 2]
+print(dropped.shape[0])
 
+# Number of rows remained
+df2 = df2.loc[df2['exam_status'] == 2]
+t_count = df2.shape[0]
+print(t_count)
 
+# ### part d)
 
+df2_u = df2.groupby(['ohx', 'under_20']).size()
+df2_g = df2.groupby(['ohx', 'gender']).size()
+df2_c = df2.groupby(['ohx', 'college']).size()
+df2_u_a = df2.groupby(['ohx', 'under_20'])['age'].agg(['mean','std'])
+df2_u_g = df2.groupby(['ohx', 'gender'])['age'].agg(['mean','std'])
+df2_u_c = df2.groupby(['ohx', 'college'])['age'].agg(['mean','std'])
+df2_a = pd.concat([df2_u_a, df2_u_g, df2_u_c], keys = ['under_20', 'gender',
+                                                       'college'])
+df2_a = df2_a.unstack(level = 1)
 
+cat = pd.concat([df2_u, df2_g, df2_c],
+                keys = ['under_20', 'gender', 'college']).to_frame()
+cat.columns= [*cat.columns[:-1], 'count']
+cat = cat.unstack(level=1)
+cat['complete%'] = round(cat.iloc[:, 0]/cat.sum(axis=1)*100,3)
+cat['missing%'] = round(cat.iloc[:, 1]/cat.sum(axis=1)*100,3)
+cat = cat.join(df2_a.reindex(df2_a.index, level=0))
+cat = cat.rename({'mean': 'mean_age', 'std': 'std_age'}, axis=1)
+cat['p-value(t-test)'] = cat.apply(lambda row :
+                                   ttest_ind_from_stats(mean1=row[4],
+                                                        std1=row[6],
+                                                        nobs1=row[0],
+                                                        mean2=row[5],
+                                                        std2=row[7],
+                                                        nobs2=row[1])[1]
+                                   , axis = 1)
+# cat
+
+display(HTML(cat.to_html(index=True)))
